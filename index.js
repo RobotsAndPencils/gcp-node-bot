07340ae6388ff967281bb23eb81ae2fcb2983f6b
Colin Gislason <colin.gislason@robotsandpencils.com>
2016-05-03 16:18:41 -0500

    Basic oauth flow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var Botkit = require('botkit');
-var firebaseStorage = require('botkit-storage-firebase');
+var express = require('express');
 var google = require('googleapis');
+var AuthCache = require('./lib/authcache.js');
 var gcpbot = require('./lib/bot.js');
 
 function requireEnvVariable(name) {
@@ -13,24 +14,50 @@ function requireEnvVariable(name) {
 
 // Expect a bunch of environment variables
 var slackToken = requireEnvVariable('SLACK_TOKEN');
-var private_key = requireEnvVariable('PRIVATE_KEY');
-var client_email = requireEnvVariable('CLIENT_EMAIL');
+var googleClientId = requireEnvVariable('GOOGLE_CLIENT_ID');
+var googleClientSecret = requireEnvVariable('GOOGLE_CLIENT_SECRET');
 var firebase_uri = process.env.FIREBASE_URI; // not required, defaults to JSON store
+var port = process.env.PORT || 3000; // not required, default to 3000
+var oauthRedirectUrl = process.env.OAUTH_REDIRECT_URL || 'http://localhost:' + port + '/auth'; // not required, default to localhost
 
-var jwtClient = new google.auth.JWT(client_email, null, private_key,
-  [
-    'https://www.googleapis.com/auth/ndev.cloudman',
-    'https://www.googleapis.com/auth/cloud-platform',
-    'https://www.googleapis.com/auth/monitoring',
-  ], null);
-
+var authCache = new AuthCache({ 
+  googleClientId: googleClientId, 
+  googleClientSecret: googleClientSecret, 
+  oauthRedirectUrl: oauthRedirectUrl 
+});
 var botkitOptions = { retry: true };
 if (firebase_uri) {
+  var firebaseStorage = require('botkit-storage-firebase');
   botkitOptions.storage = firebaseStorage({ firebase_uri: firebase_uri });
 } else {
   botkitOptions.json_file_store = './userdata/';
 }
 var controller = Botkit.slackbot(botkitOptions);
-gcpbot(controller, slackToken);
+gcpbot(controller, slackToken, authCache);
 
-//https://beepboophq.com/proxy/<id>/auth
+var app = express();
+app.get('/auth', function(req, res) {
+  console.log('request at /auth');
+  
+  var user = req.query.state;
+  var auth = authCache.lookupAuth(user);
+  if(auth && auth.client) {
+    var oauth2Client = auth.client;
+    oauth2Client.getToken(req.query.code, function(err, tokens) {
+      if(!err) {
+        oauth2Client.setCredentials(tokens);
+        authCache.saveAuth(auth);
+      } else {
+        console.log('error authorizing...', err);
+      }
+    });
+    res.send('You have been authenticated...');
+  } else {
+    console.error('requesting auth for user with no client...', user);
+    res.send('There was a problem trying to authenticate you.');
+  }
+});
+
+app.listen(port, function () {
+  console.log('GCPBot listening on port:', port);
+});
